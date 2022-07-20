1c1e9a894aef762b93599a5d35d412f73e3af332
Quentinx6 <82029618+Quentinx6@users.noreply.github.com>
2022-07-20 10:52:21 +0200

    list ok and flag ok

--- script.js
+++ script.js
@@ -6,21 +6,45 @@ fetch("https://ergast.com/api/f1/2022.json")
     .then((data)=>{
         console.log(data)
         let container = document.getElementsByClassName("container")[0];
-        let div1 = document.createElement("div");
-        div1.setAttribute("class", "div1");
+        // let div1 = document.createElement("div");
+        // div1.setAttribute("class", "div1");
 
-        let titleData = data["MRData"]["RaceTable"]["Races"]["0"]["raceName"]
-        let titlePara = document.createElement("h1");
+        // let titleData = data["MRData"]["RaceTable"]["Races"]["0"]["raceName"]
+        // let titlePara = document.createElement("h1");
 
-        titlePara.innerHTML = titleData
+        for (let i = 0; i < data["MRData"]["RaceTable"]["Races"].length; i++){
+            // console.log(data["MRData"]["RaceTable"]["Races"][i]["raceName"])
+            let divParent = document.createElement("div");
+            divParent.setAttribute("class", "divParent");
+            let divGp = document.createElement("div");
+            divGp.setAttribute("class", "divGp");
+            let flagGp = document.createElement("img");
+            flagGp.setAttribute("class", "flagGp");
+            flagGp.setAttribute("src", "image/" + i +".png");
+            let pGp = document.createElement("p");
+            pGp.setAttribute("class", "pGp");
+            let chevron = document.createElement("img");
+            chevron.setAttribute("class","chevron");
+            chevron.setAttribute("src", "image/chevron.png");
+            let pData = data["MRData"]["RaceTable"]["Races"][i]["raceName"];
+            pGp.innerHTML = pData;
+            container.appendChild(divParent);
+                divParent.appendChild(divGp)
+                divGp.appendChild(flagGp);
+                divGp.appendChild(pGp);
+                divParent.appendChild(chevron);
+        }
 
-        let circuitData = data["MRData"]["RaceTable"]["Races"]["0"]["Circuit"]["circuitName"]
-        let circuitPara = document.createElement("p");
+        // titlePara.innerHTML = titleData
 
-        circuitPara.innerHTML = circuitData
+        // let circuitData = data["MRData"]["RaceTable"]["Races"]["0"]["Circuit"]["circuitName"]
+        // let circuitPara = document.createElement("p");
 
-        container.appendChild(div1);
-            div1.appendChild(titlePara);
-            div1.appendChild(circuitPara);
+        // circuitPara.innerHTML = circuitData
+
+        // container.appendChild(div1);
+        //     div1.appendChild(titlePara);
+
+            // div1.appendChild(circuitPara);
             
     })
