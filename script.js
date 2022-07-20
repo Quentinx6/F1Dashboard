2c18c0e8f21ca2dd8623d42781cd31d030dd1ffb
Quentinx6 <82029618+Quentinx6@users.noreply.github.com>
2022-07-20 12:05:59 +0200

    search result F1

--- script.js
+++ script.js
@@ -6,45 +6,45 @@ fetch("https://ergast.com/api/f1/2022.json")
     .then((data)=>{
         console.log(data)
         let container = document.getElementsByClassName("container")[0];
-        // let div1 = document.createElement("div");
-        // div1.setAttribute("class", "div1");
-
-        // let titleData = data["MRData"]["RaceTable"]["Races"]["0"]["raceName"]
-        // let titlePara = document.createElement("h1");
 
         for (let i = 0; i < data["MRData"]["RaceTable"]["Races"].length; i++){
-            // console.log(data["MRData"]["RaceTable"]["Races"][i]["raceName"])
+
             let divParent = document.createElement("div");
             divParent.setAttribute("class", "divParent");
+
             let divGp = document.createElement("div");
             divGp.setAttribute("class", "divGp");
+
             let flagGp = document.createElement("img");
             flagGp.setAttribute("class", "flagGp");
             flagGp.setAttribute("src", "image/" + i +".png");
+
             let pGp = document.createElement("p");
             pGp.setAttribute("class", "pGp");
+
             let chevron = document.createElement("img");
             chevron.setAttribute("class","chevron");
             chevron.setAttribute("src", "image/chevron.png");
+
             let pData = data["MRData"]["RaceTable"]["Races"][i]["raceName"];
             pGp.innerHTML = pData;
+
+            divParent.addEventListener("click", () => {
+                let divInfo = document.createElement("div");
+                divInfo.setAttribute("class", "divInfo");
+                // divInfo.classlist.toggle("display")
+            })
+
             container.appendChild(divParent);
                 divParent.appendChild(divGp)
                 divGp.appendChild(flagGp);
                 divGp.appendChild(pGp);
                 divParent.appendChild(chevron);
         }
+        fetch("https://ergast.com/api/f1/2022/results.json")
 
-        // titlePara.innerHTML = titleData
-
-        // let circuitData = data["MRData"]["RaceTable"]["Races"]["0"]["Circuit"]["circuitName"]
-        // let circuitPara = document.createElement("p");
-
-        // circuitPara.innerHTML = circuitData
-
-        // container.appendChild(div1);
-        //     div1.appendChild(titlePara);
-
-            // div1.appendChild(circuitPara);
-            
+        .then((res) => res.json())
+        .then((results) =>{
+            console.log(results)
+        })
     })
